0d6eb5a6de6b27ef04498009c860c71776e58482
PatricioPoncini <patricioponcini10@gmail.com>
2024-07-21 12:49:04 -0300

    feat: auth middleware

--- src/routes/auth.routes.ts
+++ src/routes/auth.routes.ts
@@ -1,9 +1,10 @@
 import { Router } from "express";
 import { loginUser, refreshToken } from "../controllers/auth.controller";
+import { authMiddleware } from "../middlewares/auth";
 
 const router = Router();
 
 router.post("/login", loginUser);
-router.post("/refresh-token", refreshToken);
+router.post("/refresh-token", authMiddleware, refreshToken);
 
 export default router;
